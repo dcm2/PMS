81d4ae9eacb50893124abd9095b0c934e4d113a9
unnur <uab1@hi.is>
2018-10-31 11:52:03 +0200

    Entities Song and SongInfo implemented

--- src/main/java/project/persistence/entities/Song.java
+++ src/main/java/project/persistence/entities/Song.java
@@ -8,10 +8,50 @@ import javax.persistence.Id;
 @Entity
 public class Song {
 
+	//private Long id;
+
+	private int votes;
+	private int upVotes;
+	private int downVotes;
+	private SongInfo songInfo;
+	
+	// Constructor
+	public Song(SongInfo songInfo) {
+		this.setSongInfo(songInfo);
+	}
+	
+	// Getters and setters
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
-	private Long id;
-	
+	public int getVotes() {
+		return votes;
+	}
 
+	public void setVotes(int votes) {
+		this.votes = votes;
+	}
 	
+	public int getUpVotes() {
+		return upVotes;
+	}
+
+	public void setUpVotes(int upVotes) {
+		this.upVotes = upVotes;
+	}
+	
+	public int getDownVotes() {
+		return downVotes;
+	}
+
+	public void setDownVotes(int downVotes) {
+		this.downVotes = downVotes;
+	}
+
+	public SongInfo getSongInfo() {
+		return songInfo;
+	}
+
+	public void setSongInfo(SongInfo songInfo) {
+		this.songInfo = songInfo;
+	}
 }
--- src/main/java/project/persistence/entities/SongInfo.java
+++ src/main/java/project/persistence/entities/SongInfo.java
@@ -1,5 +1,74 @@
 package project.persistence.entities;
 
-public class SongInfo {
+import javax.persistence.Entity;
 
+@Entity
+public class SongInfo {
+	
+	private Long id;
+	
+	private String title;
+	private String artist;
+	private int length;
+	private String album;
+	private String genre;
+	private String pathToFile;
+	
+	// Constructor
+	public SongInfo(String title, String artist, int length, String genre) {
+		this.setTitle(title);
+		this.setArtist(artist);
+		this.setLength(length);
+		this.setGenre(genre);
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public String getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getPathToFile() {
+		return pathToFile;
+	}
+
+	public void setPathToFile(String pathToFile) {
+		this.pathToFile = pathToFile;
+	}
+	
+	
 }
