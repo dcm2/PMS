eaf9510343f515a22e64090c6e845453c76f45ea
unnur <uab1@hi.is>
2018-10-31 12:25:49 +0200

    Test

--- src/main/java/project/persistence/entities/Song.java
+++ src/main/java/project/persistence/entities/Song.java
@@ -54,4 +54,5 @@ public class Song {
 	public void setSongInfo(SongInfo songInfo) {
 		this.songInfo = songInfo;
 	}
+	
 }
--- src/main/java/project/persistence/entities/SongInfo.java
+++ src/main/java/project/persistence/entities/SongInfo.java
@@ -70,5 +70,4 @@ public class SongInfo {
 		this.pathToFile = pathToFile;
 	}
 	
-	
 }
